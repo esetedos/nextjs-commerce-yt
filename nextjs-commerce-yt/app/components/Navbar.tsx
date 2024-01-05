31d2e4b60c8d46e39e302e04a5b4297bf9645ab8
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-05 12:36:27 +0100

    [ADD] shopping button appearance

--- nextjs-commerce-yt/app/components/Navbar.tsx
+++ nextjs-commerce-yt/app/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
 
 const links = [
     { name: "Home", href: "/" },
@@ -44,8 +45,14 @@ export default function Navbar() {
                 ))}
                 </nav>
                 <div className="flex divide-x border-r sm:border-l">
-                    <Button className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none">
-                    button
+                    <Button
+                        variant={"outline"}
+                        className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
+                    >
+                        <ShoppingBag />
+                        <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+                        Cart
+                        </span>
                     </Button>
                 </div>
             </div>
