a462b3fac539f7654124f27269d90e80e8942ee1
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-05 19:38:50 +0100

    [ADD] category page done

--- nextjs-commerce-yt/app/product/[slug]/page.tsx
+++ nextjs-commerce-yt/app/product/[slug]/page.tsx
@@ -76,26 +76,10 @@ export default async function ProductPge({
               <span className="text-sm">2-4 Day Shipping</span>
             </div>
 
-            {/* <div className="flex gap-2.5">
-              <AddToBag
-                currency="USD"
-                description={data.description}
-                image={data.images[0]}
-                name={data.name}
-                price={data.price}
-                key={data._id}
-                price_id={data.price_id}
-              />
-              <CheckoutNow
-                currency="USD"
-                description={data.description}
-                image={data.images[0]}
-                name={data.name}
-                price={data.price}
-                key={data._id}
-                price_id={data.price_id}
-              />
-            </div> */}
+            <div className="flex gap-2.5">
+              <Button>Add To Bag</Button>
+              <Button variant={"secondary"}>Checkout now</Button>
+            </div>
 
             <p className="mt-12 text-base text-gray-500 tracking-wide">
               {data.description}
