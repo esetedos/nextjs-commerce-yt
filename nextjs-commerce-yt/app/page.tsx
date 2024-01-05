ff35dfc5ff6645030ee74d9fcd9697c493216688
esetedos <esther.fernandez@ikasle.aeg.eus>
2024-01-05 13:10:54 +0100

    [ADD] images on home page

--- nextjs-commerce-yt/app/page.tsx
+++ nextjs-commerce-yt/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1> hello from da main page</h1>
+    <div className="bg-white pd-6 sm:pb-8 lg-12">
+      <Hero />
     </div>
   )
 }
